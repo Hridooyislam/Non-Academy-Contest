a5f14ff9974046fd3d1e701c2b4ddce685bb96ee
Hridooyislam <you@example.comislam111hridoy@gmail.com>
2023-08-04 04:15:15 +0600

    Update-1

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    
+    theme: {
+      // screens: {
+      //   'sm_1': '320px',
+      //   'sm_2': '375px',
+      //   'sm_3': '425px',
+      //   // => @media (min-width: 640px) { ... }
+  
+      //   'md': '768px',
+      //   // => @media (min-width: 768px) { ... }
+  
+      //   'lg': '1024px',
+      //   // => @media (min-width: 1024px) { ... }
+  
+      //   'xl': '1280px',
+      //   // => @media (min-width: 1280px) { ... }
+  
+      //   '2xl': '1536px',
+      //   // => @media (min-width: 1536px) { ... }
+      // },
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  
+
+  
